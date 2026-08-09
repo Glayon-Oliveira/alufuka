bb2e2507b45949604aad412aea0bac7822ce1ebb
Glayon Oliveira <glayonsantana@gmail.com>
2026-08-08 22:33:56 -0300

    Implements socket as main comunication

--- java-service/src/main/java/com/lmlasmo/alufuka/App.java
+++ java-service/src/main/java/com/lmlasmo/alufuka/App.java
@@ -1,9 +1,20 @@
 package com.lmlasmo.alufuka;
 
+import com.lmlasmo.alufuka.comunication.SocketReceptor;
+
 public class App {
 
 	public static void main(String[] args) throws Exception {
 		ContextConfiguration.configure();
+		Context.setRECEPTOR(new SocketReceptor(5130));
+		
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			try {
+				Context.receptor().stop();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}));
 		
 		Context.receptor().start();
 	}
---
+++ java-service/src/main/java/com/lmlasmo/alufuka/comunication/SocketReceptor.java
@@ -0,0 +1,70 @@
+package com.lmlasmo.alufuka.comunication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.lmlasmo.alufuka.Context;
+import com.lmlasmo.alufuka.executor.Executor;
+import com.lmlasmo.alufuka.executor.Result;
+
+public class SocketReceptor implements Receptor {
+	
+	private ExecutorService pool = Executors.newScheduledThreadPool(100);
+	private boolean running = false;
+	private int port;
+	
+	public SocketReceptor(int port) throws IOException {
+		this.port = port;
+	}
+
+	@Override
+	public void start() throws Exception {
+		running = true;
+				
+		Executor executor = new Executor();
+		
+		System.out.println("Java service started");
+		
+		try(ServerSocket server = new ServerSocket(port)) {
+			System.out.println("Java service listening on port " + port);
+			
+			while(running) {
+				Socket socket = server.accept();
+				
+				pool.execute(() -> {
+					try(BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+							PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+						
+						String next;
+						
+						while((next = reader.readLine()) != null) {
+							System.out.println("Receive: " + next);
+							Result result = executor.execute(next.getBytes());
+							
+							String strResult = Context.objectMapper().writeValueAsString(result);
+							
+							writer.println(strResult);
+						}
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+				});
+			}
+		}
+	}
+
+	@Override
+	public void stop() throws Exception {
+		running = false;
+		pool.shutdown();
+		
+		System.out.println("Java service terminated");
+	}
+
+}
